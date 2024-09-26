f65fc4e150ca5a9b778830bafbead6a35d69b65b
Jed Rembold <jjrembold@willamette.edu>
2024-09-25 17:37:47 -0700

    Update PDF questions and template names to match reordering

--- Prob1.py
+++ Prob1.py
@@ -1,28 +1,26 @@
+#############################################################
+# Section practice working with the WordleGWindow methods
+#############################################################
 
+from WordleGraphics import WordleGWindow, N_ROWS, N_COLS
+from english import ENGLISH_WORDS, is_english_word
+from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR
 import random
 
-def generate_password(length, characters):
-    """
-    Generates a password of the desired length with the provided characters
+def wordle():
+    """ The main function to play the Wordle game. """
 
-    Args:
-        length (int): the length of the password in number of characters
-        characters (str): the string of possible characters to use in the password
-    Returns:
-        (str): the desired password
-    """
-    # Your code here!
+    def enter_action():
+        """ What should happen when the RETURN or ENTER key is pressed. """
+        gw.show_message("Smile!")
 
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
 
 
 
-if __name__ == '__main__':
 
-    import string
-
-    chars = string.ascii_letters
-    print(f"Simple password:  {generate_password(5, chars)}")
-    chars += string.digits
-    print(f"Medium password:  {generate_password(10, chars)}")
-    chars += string.punctuation
-    print(f"Complex password: {generate_password(15, chars)}")
+# Startup boilerplate
+if __name__ == "__main__":
+    wordle()
